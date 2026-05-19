bee10fba7ef48a4e790d732c8be46e1d95ec3274
lelik <lelik.guru@gmail.com>
2026-05-19 17:45:24 +0300

    7.3 Сценарные тесты HTTP API

--- tests/suites/http/test_gateway.py
+++ tests/suites/http/test_gateway.py
@@ -123,28 +123,3 @@ class TestGatewayHTTP:
             response
         )
 
-
-    @allure.story(AllureStory.GET_USER_DETAILS)
-    @allure.title("[HTTP] Get user details. User with active debit card account")
-    def test_get_user_details_user_with_active_debit_card_account(
-        self,
-        gateway_http_test_client: GatewayHTTPTestClient,
-    ):
-        """
-        Сценарий:
-        Пользователь с активным дебетным счётом
-        запрашивает агрегированные данные через gateway-service.
-
-        Управляющая переменная теста — сценарий.
-        Все данные внешнего мира выбираются через него.
-        """
-
-        response = gateway_http_test_client.get_user_details(
-            RequestContext(
-                scenario=Scenario.USER_WITH_ACTIVE_DEBIT_CARD_ACCOUNT
-            )
-        )
-
-        assert_get_user_details_response_user_with_active_debit_card_account(
-            response
-        )
